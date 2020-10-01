dc6fbe42a2c618f82d825d37c29d5a1989667c17
Juhasz Peter <b.juhaszpeter@gmail.com>
2020-10-01 06:50:39 +0200

    add files

---
+++ Program.cs
@@ -0,0 +1,21 @@
+//----------- Console Creatures ----------------
+
+
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      //Write out consolt Creatures
+      Console.WriteLine(" .-.");    
+      Console.WriteLine("(o o)"); 
+      Console.WriteLine("| O |"); 
+      Console.WriteLine("|   |"); 
+      Console.WriteLine("'~~~'"); 
+      Console.WriteLine("Spiro the Specter");
+     }
+  }
+}
